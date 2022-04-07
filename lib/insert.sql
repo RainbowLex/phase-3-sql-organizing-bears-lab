0847d4ea0ff954329ca94422e9e26d60dceece63
Alexia Velador <alexiavel01@gmail.com>
2022-04-07 10:48:14 -0600

    commit tree 93ec544b197db1d9121c3b7b7504fa0adb11ea24

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color) VALUES ('Mr.Chocolate',4,'M','Brown');
+INSERT INTO bears (name,age,sex,color) VALUES ('Rowdy',1,'F','White');
+INSERT INTO bears (name,age,sex,color) VALUES ('Tabitha',7,'F','Tan');
+INSERT INTO bears (name,age,sex,color) VALUES ('Sergeant Brown',11,'M','Brown');
+INSERT INTO bears (name,age,sex,color) VALUES ('Melissa',2,'F','Black');
+INSERT INTO bears (name,age,sex,color) VALUES ('Grinch',10,'M','Green');
+INSERT INTO bears (name,age,sex,color) VALUES ('Wendy',8,'F','white');
+INSERT INTO bears (name,age,sex,color) VALUES (NULL,4,'M','Black');
